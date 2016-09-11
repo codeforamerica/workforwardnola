2a99a8a4eae5b1bc96e580977a058f8a57150d88
Georgia Andrews <georgia@codeforamerica.org>
2016-09-10 22:41:00 -0500

    mostly linter changes

--- views/careers.rb
+++ views/careers.rb
@@ -11,7 +11,7 @@ module WorkForwardNola
 
       def career_descriptions
         # get all the "me" traits
-        it_me = @quiz_answers.select{|trait, ans| ans.eql? 'me'}.keys
+        it_me = @quiz_answers.select { |trait, ans| ans.eql? 'me' }.keys
         # only select careers that match the "me" traits
         @career_matches = Career.where(traits: Trait.where(name: it_me))
                                 .map do |career|
@@ -26,16 +26,16 @@ module WorkForwardNola
           }
         end
 
-        @career_matches = @career_matches.sort_by{|career| career[:match_score]}
-                                        .reverse
-                                        .first(3)
+        @career_matches = @career_matches.sort_by { |career| career[:match_score] }
+                                         .reverse
+                                         .first(3)
 
         @career_matches
       end
 
       private
 
-      def trait_match_score user_trait_names, career_traits
+      def trait_match_score(user_trait_names, career_traits)
         score = 0
 
         # loop through traits of the career and see how many match the ones the user selected
@@ -49,7 +49,7 @@ module WorkForwardNola
       end
 
       # expecting float
-      def to_money amount
+      def to_money(amount)
         sprintf '$%.2f', amount
         # if needed, for putting commas in money format:
         # "$#{amount.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, '\0,')}"
