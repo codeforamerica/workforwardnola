712d11edd13c8db686be04f6355e7779b703f3c2
Todd Loupe <maahern@loyno.edu>
2017-11-02 20:59:49 +0000

    Salary update

--- views/assessment.rb
+++ views/assessment.rb
@@ -4,61 +4,80 @@ module WorkForwardNola
     class Assessment < Layout
       attr_reader :title
 
+      #reverse_trait is an optional feature, used for a 'not me' result
       def images
         [
-          { trait: 'Following instructions', # must match name of trait, 'me' result
-            reverse_trait: 'Self-starter', # optional, 'not me' result
-            title: 'Following instructions', # text to display with image
+          { trait: 'Problem solver', # must match name of trait, 'me' result
+            reverse_trait: 'Following instructions', # optional, 'not me' result
+            title: 'Solving problems', # text to display with image
             alt: 'Person is looking at a recipe on a computer before they begin chopping vegetables', # text describing image, for accessibility
             file: 'instructions.jpg' }, # file name of image in /assets/images/assessment
           { trait: 'Working with your hands',
-            reverse_trait: 'Technical',
-            title: 'Working with your hands',
+            reverse_trait: 'Work on ground',
+            title: 'Working with your hands?',
             alt: 'Person has a screwdriver and wrench in their hands and is working on mechanical parts',
             file: 'withhands.jpg' },
           { trait: 'Teamwork',
-            reverse_trait: 'Researcher',
+            reverse_trait: 'Helpful',
             title: 'Working on a team',
             alt: 'Two people are looking at a computer screen together',
             file: 'teamwork.jpg' },
-          { trait: 'Working well under pressure',
-            title: 'Working under a deadline',
+          { trait: 'Learner',
+            reverse_trait: 'Helpful',
+            title: 'Learning new things',
             alt: 'A person checks their watch while working on a computer. They are surrounded by papers',
             file: 'underpressure.jpg' },
-          { trait: 'Customer service',
-            title: 'Working with customers',
+          { trait: 'Following instructions',
+            reverse_trait: 'Open-minded',
+            title: 'Following instructions',
             alt: 'Person is sitting behind a desk helping a customer fill out paperwork',
             file: 'customerservice.jpg' },
-          { trait: 'Repetition',
-            reverse_trait: 'Adaptable',
-            title: 'Following a routine every day',
+          { trait: 'Outgoing personality',
+            reverse_trait: 'Individualistic',
+            title: 'Outgoing personality',
             alt: 'Person is driving a bus',
             file: 'routine.jpg' },
-          { trait: 'Organized',
-            title: 'Being well-organized',
+          { trait: 'Work outdoors',
+            reverse_trait: 'Work on ground',
+            title: 'Work outdoors',
             alt: 'Picture of a well-organized closet',
             file: 'organized.jpg' },
           { trait: 'No fear of heights',
+            reverse_trait: 'Work on ground',
             title: 'Comfortable with heights',
             alt: 'Person is working on power lines several feet off the ground',
             file: 'heights.jpg' },
           { trait: 'Detail oriented',
+            reverse_trait: 'Physical Strength',
             title: 'Paying attention to details',
+            alt: 'Person is using a magnifying glass to look over documents',
+            file: 'detail.jpg' },
+          { trait: 'Physical Strength',
+            reverse_trait: 'Helping people',
+            title: 'Physical strength',
+            alt: 'Person is using a magnifying glass to look over documents',
+            file: 'detail.jpg' },
+          { trait: 'Helping people',
+            reverse_trait: 'Physical Strength',
+            title: 'Helping people',
             alt: 'Person is using a magnifying glass to look over documents',
             file: 'detail.jpg' }
         ]
       end
 
+
       def compound_traits
         [
-          { trait: 'Perceptive', # name of trait
-            components: ['Detail oriented', 'Customer service'] }, # these traits are in the images list above
+          { trait: 'Open-minded', # name of trait
+            components: ['Problem solver', 'Learner'] }, # these traits are in the images list above
           { trait: 'Physical Strength',
             components: ['Working with your hands', 'No fear of heights'] },
-          { trait: 'Reliable',
-            components: ['Working well under pressure', 'Organized'] },
-          { trait: 'Leadership',
-            components: ['Teamwork', 'Self-starter'] }
+          { trait: 'Work on ground',
+            components: ['Learner', 'Problem solver', 'Detail oriented', 'Outgoing personality', 'Following Instructions', 'Helping people'] },
+          { trait: 'Individualistic',
+            components: ['Learner', 'Problem solver', 'Working outdoors', 'Working with your hands', 'No fear of heights'] },
+          { trait: 'Helpful',
+            components: ['Outgoing personality', 'Following instructions', 'Helping people'] }
         ]
       end
     end
