d2373422b6c0e605e0191110e0db84278fd5a42e
Todd Loupe <maahern@my.loyno.edu>
2017-11-06 14:23:35 +0000

    Entry_wage added and HTML updated for both entry_wage and experienced_range

--- views/careers.rb
+++ views/careers.rb
@@ -69,11 +69,11 @@ module WorkForwardNola
       end
 
       # expecting float
-      def to_money(amount)
-        sprintf '$%.2f', amount
+     # def to_money(amount)
+      #  sprintf '$%.2f', amount
         # if needed, for putting commas in money format:
         # "$#{amount.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, '\0,')}"
-      end
+      #end
     end
   end
 end
